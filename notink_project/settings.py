f9b89b415585e6ebbe0f947fcfca008ff4eec5e0
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-09-08 16:58:27 +0100

    Add navigation to the homepage

--- notink_project/settings.py
+++ notink_project/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
