d71c8446e83ae39183874c3b5db23b0e93e6c09a
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-09-29 10:12:14 +0100

    Create tests for signup and home pages
    Tested the status codes
    Tested the template used
    Tested the signup form

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpPageTests(TestCase):
+
+	username = 'newuser'
+	email = 'newuser@email.com'
+
+	def test_signup_page_status_code(self):
+		response = self.client.get('/accounts/signup/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_url_by_name(self):
+		response = self.client.get(reverse('signup'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		response = self.client.get(reverse('signup'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'signup.html')
+
+	def test_signup_form(self):
+		new_user = get_user_model().objects.create_user(
+			self.username, self.email
+		)
+		self.assertEqual(get_user_model().objects.all().count(), 1)
+		self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+		self.assertEqual(get_user_model().objects.all()[0].email, self.email)
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+	def test_home_page_status_code(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_url_by_name(self):
+		response = self.client.get(reverse('home'))
+		self.assertEqual(response.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		response = self.client.get(reverse('home'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'home.html')
