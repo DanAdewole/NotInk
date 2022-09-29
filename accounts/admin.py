56da85e24b40673329366bfcb2592b17910b60ae
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-09-29 10:20:40 +0100

    Add user authentication with minimal styling

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
+	add_form = CustomUserCreationForm
+	form = CustomUserChangeForm
 	model = CustomUser
 	list_display = ['email', 'username', 'is_staff']
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+
+from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
-	form_class = UserCreationForm
+	form_class = CustomUserCreationForm
 	success_url = reverse_lazy('login')
 	template_name = 'signup.html'
