d8421163814164a723570ddf6679ac5729a40b90
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-10-20 11:28:57 +0100

    Change the db to postgress

--- notink_project/settings.py
+++ notink_project/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'notink_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
