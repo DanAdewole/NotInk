718a9648fa211b1d8da3d084756bb9e55236fbb5
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-10-21 22:12:03 +0100

    Change db to railway postgres

--- notink_project/settings.py
+++ notink_project/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'notink_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
